d85e550b3a4f89330e426a55fef9e9fa1969b78d
Gabriel <gabrielfurlan05@gmail.com>
2021-02-08 10:43:09 -0300

    removing redirect validation in initial user fetch

--- components/main/main.controller.js
+++ components/main/main.controller.js
@@ -10,13 +10,6 @@ import { getUser, createUserSOM } from './main.repository';
 export const fetchLoggedUser = async (ida, dispatch, router) => {
   let response = await getUser(ida);
 
-  let user = response.data.oneUser;
-  if (!user) {
-    router.push('/welcome');
-  } else if (router.pathname === '/') {
-    router.push('/wall');
-  }
-
   // cria um novo usuário S.O.M caso não seja encontrado
   if (!user) {
     response = await createUserSOM(ida);
