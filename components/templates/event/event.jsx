ffd36953e5646dc92f3992d03b44cb17a9714e5e
Mariane <a.marianesantana@gmail.com>
2021-07-13 22:20:55 -0300

    som2021-56 - nao aparecer artistas e produtores que nào estiverem inscritos na oportunidade

--- components/templates/event/event.jsx
+++ components/templates/event/event.jsx
@@ -106,7 +106,7 @@ const EventPage = () => {
 
     return subscribed;
   };
-
+console.log(event)
   return (
     <Store.Consumer>
       {({ state, dispatch }) => (
@@ -133,6 +133,7 @@ const EventPage = () => {
               diffDays={closingDiffDays}
               diffHours={closingDiffHours}
               loggedAs={myState.connectionType}
+
               subscribedArtists={event.subscribers.length}
               subscribedProductors={event.subscribed_productors.length}
               subscribeAction={() =>
@@ -156,16 +157,20 @@ const EventPage = () => {
               <ProductorCardWrapper>
                 <ProductorCard productor={event.productor} router={router} />
               </ProductorCardWrapper>
-              <SubscribedArtists
-                artistClick={(artistId) => router.push(`/artist/${artistId}`)}
-                artists={event.subscribers}
-                approveds={event.approved_artists}
-              />
-              <SubscribedProductors
-                productorClick={(productorId) => router.push(`/producer/${productorId}`)}
-                productors={event.subscribed_productors}
-                approveds={event.approved_productors}
-              />
+              {event.approved_artists.length ? (
+                <SubscribedArtists
+                  artistClick={(artistId) => router.push(`/artist/${artistId}`)}
+                  artists={event.subscribers}
+                  approveds={event.approved_artists}
+                />
+              ) : null}
+              {event.approved_productors.length ? (
+                <SubscribedProductors
+                  productorClick={(productorId) => router.push(`/producer/${productorId}`)}
+                  productors={event.subscribed_productors}
+                  approveds={event.approved_productors}
+                />
+              ) : null}
             </ColumnWrapper>
           </Content>
           {dialog.title ? (
