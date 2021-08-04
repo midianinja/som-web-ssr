3c3e2533dd21fd61f406ddcdb9f8c505c30fe9e6
Isaque Costa <isaquebc@gmail.com>
2021-08-04 11:53:04 -0300

    Testing deploy

---
+++ deploy/scripts/application_start.sh
@@ -0,0 +1,3 @@
+
+cp -r /home/ubuntu/som-web/dist/apps/accounts /usr/share/nginx/som-web/
+sudo /etc/init.d/nginx reload
---
+++ deploy/scripts/before_install.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+#download node and npm
+rm -rf /home/ubuntu/som-web/dist/*
+rm -rf /usr/share/nginx/som-web/*
+rm /etc/nginx/sites-enabled/*
