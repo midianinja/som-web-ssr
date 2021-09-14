1b0f6754a04730d975c12f59a6a526be0caac749
Isaque Costa <isaquebc@gmail.com>
2021-09-14 12:09:02 -0300

    Fixing deploy

--- deploy/scripts/application_start.sh
+++ deploy/scripts/application_start.sh
@@ -1,6 +1,7 @@
+sudo chown -R ubuntu.ubuntu /home/ubuntu/som-web
 cd /home/ubuntu/som-web/
-npm run build
 
 sudo /etc/init.d/nginx reload
 sudo systemctl daemon-reload
 sudo systemctl start som-web-ssr
+
